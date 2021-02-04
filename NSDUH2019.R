823c751fde88cc1ba4eed0b3685f0d8482178400
Sunmee Kim <57221141+QuantMM@users.noreply.github.com>
2021-02-04 15:41:45 -0600

    Update NSDUH2019.R

--- NSDUH2019.R
+++ NSDUH2019.R
@@ -3,10 +3,13 @@ ls()
 D <- PUF2019_100920
 
 NSUDH2019 <- subset(D, select=
-c("ndssansp", "ndssdnsp", "ftnddnsp", "dnicnsp", "cigavgm" 	# Nicotine Dep
-, "depndalc", "abusealc", "abodalc", "alcyrtot"			# Alcohol
+c("QUESTID2"                                                # ID info
+, "ndssansp", "ndssdnsp", "ftnddnsp", "dnicnsp", "cigavgm" 	# Nicotine Dep
+, "depndalc", "abusealc", "abodalc", "alcyrtot"			        # Alcohol
 , "depndmrj", "abodmrj", "mrdaypyr", "mrdaypmo", "mjyrtot"	# Marijuana
 
 # continue here as much as you want#
 
 ))
+
+write.csv(NSUDH2019,"NSUDH2019.csv", row.names = FALSE)
